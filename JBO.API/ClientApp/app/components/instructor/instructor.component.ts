0cc5880defa9045ea6457350c495499ffb3e400e
Chris Boyd <chris.boyd@maritzcx.com>
2018-04-09 16:27:15 -0400

    WIP: Update single quotes to double quotes

--- JBO.API/ClientApp/app/components/instructor/instructor.component.ts
+++ JBO.API/ClientApp/app/components/instructor/instructor.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { InstructorService } from '../../services/instructor.service';
+import { Component, OnInit } from "@angular/core";
+import { InstructorService } from "../../services/instructor.service";
+import { Instructor } from "../../models/instructor";
 
 @Component({
-    selector: 'app-instructor',
-    templateUrl: './instructor.component.html',
-    styleUrls: ['./instructor.component.css']
+    selector: "app-instructor",
+    templateUrl: "./instructor.component.html",
+    styleUrls: ["./instructor.component.css"]
 })
 export class InstructorComponent implements OnInit {
 
--- JBO.API/ClientApp/app/services/instructor.service.ts
+++ JBO.API/ClientApp/app/services/instructor.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core"
 import { Instructor } from "../models/instructor";
 
 @Injectable()
